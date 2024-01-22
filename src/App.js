cc89e4f4906ff541e6638feddef9b952d356521e
poojameda368 <157340878+poojameda368@users.noreply.github.com>
2024-01-22 10:41:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello World !</h1>
+      <h2> Roll No: 2211cs010368</h2>
+      <h2> Name: Pooja</h2>
+      <h2> CGPA: 8.64</h2>
+
+    </div>
+  );
+}
+
+export default App;
